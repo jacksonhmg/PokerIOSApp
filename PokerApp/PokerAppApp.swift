13324674a5b4158d6a671220e187ccb10d86a84c
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-20 08:52:00 -0700

    Initial Commit

---
+++ PokerApp/PokerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAppApp.swift
+//  PokerApp
+//
+//  Created by jackson mowatt gok on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
